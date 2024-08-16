3493eb1d9c6528c3f12f19fc7b9ce1000107c8f2
luccas costa <lucaspcosta70@gmail.com>
2024-08-16 15:55:32 -0300

    deletebd finsh

--- src/components/assets/ConfirmacaoSpotylightTrash.tsx
+++ src/components/assets/ConfirmacaoSpotylightTrash.tsx
@@ -11,17 +11,33 @@ interface ConfirmacaoSpotylightTrashProps {
   handlerDeleteBD: (id: number) => void;
 }
 
-export default function ConfirmacaoSpotylightTrash({ onClose, handlerDeleteBD, id , isLoading}: ConfirmacaoSpotylightTrashProps) {
+export default function ConfirmacaoSpotylightTrash({
+  onClose,
+  handlerDeleteBD,
+  id,
+  isLoading,
+}: ConfirmacaoSpotylightTrashProps) {
   return (
     <div className='absolute top-0 left-0 w-full h-full bg-black/50 flex items-center justify-center z-50'>
-      <div
-        className='relative flex flex-col space-y-4 transition-all duration-100'
-      >
-        <div className="text-2xl text-zinc-300 font-semibold">{isLoading ? "Apagando..." : "Certeza que deseja apagar essa senha?"}</div> <div className="text-2xl text-zinc-300 font-semibold">Tem certeza que deseja apagar essa senha?</div>
-        <div className="flex space-x-4">
-            <button className='bg-neutral-800/80 w-[440px] p-6 rounded-full shadow-2xl text-zinc-300' onClick={() => id !== undefined && handlerDeleteBD(id)}
-            >Apagar</button>
-            <button className='bg-neutral-800/80 w-[440px] p-6 rounded-full shadow-2xl text-zinc-300' onClick={onClose}>Cancelar</button>
+      <div className='relative flex flex-col space-y-4 transition-all duration-100'>
+        <div className='text-3xl text-zinc-300 font-semibold text-center'>
+          {isLoading
+            ? "Apagando..."
+            : "Tem certeza que deseja apagar essa senha?"}
+        </div>
+        <div className='flex space-x-4'>
+          <button
+            className='bg-red-900/70 hover:bg-red-900/90 w-[340px] p-6 rounded-2xl shadow-2xl text-zinc-300'
+            onClick={() => id !== undefined && handlerDeleteBD(id)}
+          >
+            Apagar
+          </button>
+          <button
+            className='bg-green-900/70 hover:bg-green-900/90 w-[340px] p-6 rounded-2xl shadow-2xl text-zinc-300'
+            onClick={onClose}
+          >
+            Cancelar
+          </button>
         </div>
       </div>
     </div>
--- src/utils/deleteBD.ts
+++ src/utils/deleteBD.ts
@@ -5,12 +5,12 @@ import { sql } from "@vercel/postgres";
 // Função para apagar um registro da tabela bdimages com base no id
 export async function deleteSenhaBD(id: number): Promise<void> {
   try {
-    console.log(`Apagando imagem com id: ${id}`);
+    console.log(`Apagando Senha com id: ${id}`);
     // Executa a consulta SQL para deletar o registro correspondente ao id fornecido
-    await sql`DELETE FROM bdimages WHERE id = ${id}`;
-    console.log("Imagem apagada com sucesso!");
+    await sql`DELETE FROM senhasbd WHERE id = ${id}`;
+    console.log("Senha apagada com sucesso!");
   } catch (error) {
-    console.log("Erro ao apagar imagem do banco de dados:", error);
-    throw new Error("Erro ao apagar imagem do banco de dados");
+    console.log("Erro ao apagar Senha do banco de dados:", error);
+    throw new Error("Erro ao apagar Senha do banco de dados");
   }
 }
